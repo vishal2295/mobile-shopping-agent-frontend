fa55f97de09c41d2d35b588ba68115090da997c2
Vishal Rai <vishal2295@gmail.com>
2025-09-28 23:10:22 +0530

    added environment variable

--- src/app/services/chat.ts
+++ src/app/services/chat.ts
@@ -3,13 +3,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ApiResponse } from '../models/chat.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
   // The URL of your Python backend
-  private backendUrl = 'https://mobile-shopping-agent-backend.vercel.app/api/chat';
+  private backendUrl = environment.backendUrl;
 
   constructor(private http: HttpClient) { }
 
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    backendUrl:'https://mobile-shopping-agent-backend.vercel.app/api/chat'
+
+};
